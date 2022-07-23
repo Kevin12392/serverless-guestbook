f72c01a1104f3d07de3315b7271317330435a0a7
Kevin12392 <108023887+Kevin12392@users.noreply.github.com>
2022-07-23 00:33:23 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://84765a6c.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
